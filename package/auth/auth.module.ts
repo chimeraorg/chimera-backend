9125f406cd13a61b0543e459eedf0f9974d6f4f8
Torkov <Torkov@archlinux.com>
2025-10-03 20:02:19 -0300

    fix(auth): fixing the import directory

--- package/auth/auth.module.ts
+++ package/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '@package/users/user.module';
 import { SecurityModule } from '@infra/security/security.module';
 import { LoginUseCase } from './application/login.use-case';
-import { AuthController } from './interface/dto/auth.controller';
+import { AuthController } from './interface/auth.controller';
 import { JwtStrategy } from './interface/dto/jwt.strategy';
 import { GenerateTokenPairUseCase } from './application/generate-token-pair.use-case';
 import { RefreshTokensUseCase } from './application/refresh-tokens.use-case';
